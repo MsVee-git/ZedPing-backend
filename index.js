9355ab2dd7f2ea700e5802e848561ec8aebd3640
MsVee-git <veronicaphiri21@gmail.com>
2026-09-10 14:11:39 +0200

    Mount protected WhatsApp connection routes

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ const automationRoutes = require('./routes/automations')
 const catalogRoutes = require('./routes/catalog')
 const orderRoutes = require('./routes/orders')
 const workspaceRoutes = require('./routes/workspace')
+const whatsappConnectionRoutes = require('./routes/whatsappConnections')
 
 app.use('/webhook', webhookRoutes)
 app.use('/messages', requireWorkspace, messageRoutes)
@@ -27,6 +28,7 @@ app.use('/automations', requireWorkspace, automationRoutes)
 app.use('/catalog', requireWorkspace, catalogRoutes)
 app.use('/orders', requireWorkspace, orderRoutes)
 app.use('/workspace', requireWorkspace, workspaceRoutes)
+app.use('/whatsapp-connections', requireWorkspace, whatsappConnectionRoutes)
 
 app.get('/', (req, res) => {
   res.json({ status: 'ZedPing backend is running' })
